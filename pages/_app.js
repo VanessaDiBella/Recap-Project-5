0a8ec9d2aaf05dbb03ad720955aa489509b77acf
Vanessa Di Bella <vanessa@di-bella.de>
2024-07-09 15:45:43 +0200

    set main branch

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import GlobalStyle from "../styles";
-
+import ArtPieces from "./art-pieces";
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
       <Component {...pageProps} />
+      <ArtPieces />
     </>
   );
 }
---
+++ pages/art-pieces/index.js
@@ -0,0 +1,5 @@
+import Card from "@/components/Card.js";
+
+export default function ArtPieces() {
+  return <Card />;
+}
